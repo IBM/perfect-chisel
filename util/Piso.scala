e28be2b5b010036bbcde7d0b82e2cdf20e6522cd
Schuyler Eldridge <schuyler.eldridge@ibm.com>
2017-01-06 16:24:18 -0500

    Add same-clock PISO
    This adds a new PISO module (Parallel-in, Serial-out). Note that this
    is just a simple SER where both sides of the SER are on the same clock
    domain.
    
    This deviates from a simple SER in that the number of bits to scan is
    included in the cmd.

---
+++ util/Piso.scala
@@ -0,0 +1,48 @@
+// See LICENSE.ibm for license details.
+package perfect.util
+
+import chisel3._
+import chisel3.util._
+
+class PisoCmd(n: Int) extends Bundle {
+  val data = UInt(n.W)
+  val count = UInt(log2Up(n).W)
+  override def cloneType = new PisoCmd(n).asInstanceOf[this.type]
+}
+
+class PisoIO(n: Int) extends Bundle {
+  val p = Decoupled(new PisoCmd(n)).flip
+  val s = Valid(Bool())
+  override def cloneType = new PisoIO(n).asInstanceOf[this.type]
+}
+
+class Piso(n: Int) extends Module with UniformPrintfs {
+  val io = IO(new PisoIO(n))
+
+  val sr = Reg(UInt(n.W))
+  val count = Reg(UInt(log2Up(n).W))
+
+  val sentCount = Reg(UInt(log2Up(n).W))
+  val hasData = Reg(init = false.B)
+  io.p.ready := !hasData
+  when (io.p.fire()) {
+    hasData := true.B
+    sr := io.p.bits.data
+    count := io.p.bits.count
+    sentCount := 0.U
+  }
+
+  io.s.valid := hasData
+  io.s.bits := sr(0)
+  when (hasData) {
+    sr := sr >> 1
+    sentCount := sentCount + 1.U
+    hasData := sentCount =/= count
+  }
+
+  when (io.p.fire()) {
+    printfInfo("Piso: Parallel bits 0x%x, count 0x%x\n", io.p.bits.data, io.p.bits.count)
+  }
+
+  assert(!(io.p.valid && !io.p.ready), "Piso: Received parallel data but not ready\n")
+}
