6f17158f95a449a0cb49e36a676aff07fa032edc
Schuyler Eldridge <schuyler.eldridge@ibm.com>
2017-05-04 19:48:17 -0400

    Add simple PWM driver
    This adds a simple Pulse Width Modulation driver. These are available
    elsewhere, but this is a good candidate for inclusion here.
    
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@ibm.com>

---
+++ src/main/scala/perfect/util/PWM.scala
@@ -0,0 +1,23 @@
+// See LICENSE.IBM for license details
+
+package perfect.util
+
+import chisel3._
+
+class PwmIo(n: Int) extends Bundle {
+  val enable    = Input(Bool())
+  val dutyCycle = Input(UInt(n.W))
+  val period    = Input(UInt(n.W))
+  val out       = Output(Bool())
+  override def cloneType: this.type = new PwmIo(n).asInstanceOf[this.type]
+}
+
+class Pwm(n: Int) extends Module {
+  val io = IO(new PwmIo(n))
+
+  val count = Reg(UInt(n.W))
+
+  io.out := io.enable && count <= io.dutyCycle
+  when (io.enable)                        { count := count + 1.U }
+  when (count >= io.period || ~io.enable) { count := 0.U         }
+}
