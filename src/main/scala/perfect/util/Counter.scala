0ef846679ea0492f75a25b6f720271808630507e
Schuyler Eldridge <schuyler.eldridge@ibm.com>
2017-05-26 18:09:01 -0400

    Add counter with run-time programmable max
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@ibm.com>

--- src/main/scala/perfect/util/Counter.scala
+++ src/main/scala/perfect/util/Counter.scala
@@ -9,6 +9,22 @@ class CounterWithReset(n: Int) extends Counter(n: Int) {
   def reset(): Unit = { value := 0.U }
 }
 
+class CounterModule(n: Int) extends Module {
+  val io = IO(new Bundle {
+    val inc = Input(Bool())
+    val extReset = Input(Bool())
+    val max = Input(UInt(log2Up(n + 1).W))
+    val wrap = Output(Bool())
+    val value = Output(UInt(log2Up(n).W))
+  })
+
+  val value = Reg(init = 0.U(n.W))
+  io.value := value
+  when (io.inc) { value := value + 1.U }
+  io.wrap := value === (io.max - 1.U)
+  when (io.extReset || io.wrap) { value := 0.U }
+}
+
 object CounterWithReset {
   def apply(n: Int): CounterWithReset = new CounterWithReset(n)
 
@@ -18,5 +34,14 @@ object CounterWithReset {
     when (cond) { wrap = c.inc() }
     when (reset) { c.reset() }
     (c.value, cond && wrap)
+  }
+
+  def apply(cond: Bool, reset: Bool, max: UInt): (UInt, Bool) = {
+    val c = Module(new CounterModule(max.getWidth))
+    c.io.inc := cond
+    c.io.extReset := reset
+    c.io.max := max
+    val Seq(value, wrap) = Seq(UInt(), Bool())
+    (c.io.value, cond && c.io.wrap)
   }
 }
