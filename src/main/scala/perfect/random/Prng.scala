85d0c1daed238d680d317f8237d859502c3f0f6f
Schuyler Eldridge <schuyler.eldridge@sifive.com>
2023-01-28 16:45:13 -0500

    Update this to work with Chisel 3.5.6
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

--- src/main/scala/perfect/random/Prng.scala
+++ src/main/scala/perfect/random/Prng.scala
@@ -19,5 +19,4 @@ import chisel3.util._
 class PrngIo(n: Int) extends Bundle {
   val seed = Input(Valid(UInt(n.W)))
   val y = Output(UInt(n.W))
-  override def cloneType: this.type = new PrngIo(n).asInstanceOf[this.type]
 }
